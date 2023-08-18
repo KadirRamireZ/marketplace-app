d7a3549c07a8e45e8fede416003130265aea3af4
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-08-18 09:07:00 -0600

    add more styles

--- src/components/page/productos/index.js
+++ src/components/page/productos/index.js
@@ -11,11 +11,11 @@ export const ProductosList = () => {
         className="nikebarra"
         src="https://d1qg2exw9ypjcp.cloudfront.net/assets/prod/127006/p1d2d0mqn35p21s441bs41oa7ifd4.gif"
       />
-      <h1 className="produ">NEW OFFERS</h1>
+      <h1 className="footer">NEW COLLECTION</h1>
       <br></br>
       <img
         className="nikebarra"
-        src="https://www.sportline.com.co/media/wysiwyg/BANNER-LANDING-DESKTOP-PADRES-PROMO-2.gif"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/def22197634807.5ec9d1a8bb14f.gif"
       />
       <div className="productos">
         {productos.map((producto) => (
